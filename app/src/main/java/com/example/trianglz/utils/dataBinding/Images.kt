cd16ecf181ec0c87b63b2de17904d6d13fed5070
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2020-09-21 21:54:54 +0200

    fix Glide add Cash for improve performance

--- app/src/main/java/com/example/trianglz/utils/dataBinding/Images.kt
+++ app/src/main/java/com/example/trianglz/utils/dataBinding/Images.kt
@@ -5,11 +5,15 @@ import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 
+val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, imageUrl: String) {
     Glide.with(view.context)
         .load(imageUrl)
+        .apply(requestOptions)
         .into(view)
 }
 
